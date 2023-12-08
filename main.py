3bc34398d2a48d5cfc822d4a4dc4b7ca8f1c90d2
Jayke Lim <jake0619@gmail.com>
2023-12-08 11:57:36 -0500

    added image selector

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-from dotenv import dotenv
+import os
+import streamlit as st
+from streamlit_image_select import image_select
+from dotenv import load_dotenv, find_dotenv
+from openai import OpenAI
+
+st.title('Vision Assist')
+load_dotenv(find_dotenv())
+#print(os.environ.get("OPENAI_API_KEY"))
+
+client = OpenAI()
+
+img = image_select(
+  label="Select an image",
+  images=[
+    "Images\Image1.png",
+    "Images\image2.jpg"
+  ],
+  captions=["First image", "Second image"]
+)
+
+st.write(str(img))
+st.image(img)
+
+# completion = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+#         {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+#     ]
+# )
+
+if st.button("Analyze Capture"):
+  st.write("Analyzing image.....")
+
